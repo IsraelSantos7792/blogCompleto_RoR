b6bc2549d706e79b08c97b48b8f62d36f7003863
IsraelSantos7792 <israelqueren1805@gmail.com>
2022-03-02 17:25:03 -0300

    add filter articles by category

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,24 @@
 # controller articles index and more
 class ArticlesController < ApplicationController
+  include Paginable
   before_action :authenticate_user!, except: %i[index show]
   before_action :set_article, only: %i[show edit update destroy]
 
   def index
-    @highlights = Article.desc_order.first(3)
-    current_page = (params[:page] || 1).to_i
+    category = Category.find_by_name(params[:category]) if params[:category].present?
+
+    @highlights = Article.filter_by_category(category)
+                         .desc_order
+                         .first(3)
+
     highlights_ids = @highlights.pluck(:id).join(',')
+
     @articles = Article.desc_order
                        .without_highlights(highlights_ids)
+                       .filter_by_category(category)
                        .page(current_page)
+
+    @categories = Category.sorted
   end
 
   def show; end
